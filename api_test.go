8f8c78d41943de063655d7fe181f0c18884831cd
Jonathan David Page <jonathan@sleepingcyb.org>
2022-04-13 05:41:52 -0400

    Address lints in api_test.go

--- api_test.go
+++ api_test.go
@@ -77,7 +77,8 @@ func TestApiRegister(t *testing.T) {
 		NotContainsKey("error")
 
 	allowfrom := map[string][]interface{}{
-		"allowfrom": []interface{}{"123.123.123.123/32",
+		"allowfrom": {
+			"123.123.123.123/32",
 			"2001:db8:a0b:12f0::1/32",
 			"[::1]/64",
 		},
@@ -116,8 +117,7 @@ func TestApiRegisterBadAllowFrom(t *testing.T) {
 
 	for _, v := range invalidVals {
 
-		allowfrom := map[string][]interface{}{
-			"allowfrom": []interface{}{v}}
+		allowfrom := map[string][]interface{}{"allowfrom": {v}}
 
 		response := e.POST("/register").
 			WithJSON(allowfrom).
